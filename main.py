692f7f534100802f357980c765354d11ab9f2b21
Arez Khidr <arezkhidr@Arezs-MacBook-Pro.local>
2025-08-19 08:56:44 -0600

    Changed CLI library to use typer and the typer-shell instead of cmd
    I made this change as with typer, it is much easier to set the options
    for commands. Further it generates the documentations for the command
    line directly from the code. Allowing for much faster usage

--- main.py
+++ main.py
@@ -1,88 +1,71 @@
-import cmd
-from os.path import join
-import redis
-import threading
-import time
-import atexit
-import signal
-import sys
+import typer
+import typer_shell
 from domain_orchestrator import DomainOrchestrator
+import redis
+from typing_extensions import Annotated
 
-# TODO: Per the Redis docuemntation, once a client is subscribed to a redis stream, it cannot execute any other commmands
-# Thereby it might be worthwile to make it such that when attempting to look at the output of a redis stream we generate a new subprocess
-# TODO: In the python redis docmentaiton there seems to be only one
+# Libraries that are used to handle graceful shutdowns
+import signal
+import atexit
+import sys
 
 
-class PyWebC2Shell(cmd.Cmd):
-    intro = "Welcome to pyWebC2 Interactive Shell. Type help or ? to list commands.\n"
-    prompt = "pyWebC2> "
-
-    def __init__(self):
-        super().__init__()
+class PyWebC2Shell:
+    def __init__(self) -> None:
+        # Initialize the typer shell as part of the class
+        self.app = typer_shell.make_typer_shell()
         self.dorch = DomainOrchestrator()
         # Redis client to be able to subscribe to the streams that are output
         self.redis_client = redis.Redis()
-        self.pubsub = self.redis_client.pubsub()
         self.monitoring = False
         self.monitor_thread = None
         # Reload all of the previously stored applications
         self.dorch.startup_applications()
-
-        # Register cleanup functions for graceful shutdown
+        # Register the shutdown handlers
         self._register_shutdown_handlers()
+        # NOTE: Any new commands that are added MUST be registered here otherwise they will not run
+        self.app.command()(self.create)
+        self.app.command()(self.remove)
+        self.app.command()(self.list)
+        self.app.command()(self.pause)
+        self.app.command()(self.resume)
+        self.app.command()(self.listen)
 
-    def do_create(self, line):
-        """Create a new domain: create <domain_name> [port]"""
-        args = line.split()
-        if not args:
-            print("Usage: create <domain_name> [port]")
-            return
+    def create(
+        self,
+        domain: str,
+        port: Annotated[
+            int,
+            typer.Option(help="The port number you would like the domain to run from"),
+        ] = 8000,
+    ):
+        """Create a new domain: create <domain> [port]"""
+        print("Usage: create <domain> [port]")
 
-        domain_name = args[0]
-        port = int(args[1]) if len(args) > 1 else None
-        self.dorch.create_domain(domain_name, ".com", port)
+        self.dorch.create_domain(domain, ".com", port)
 
-    def do_remove(self, line):
-        """Remove a domain: remove <domain_name>"""
-        if not line.strip():
-            print("Usage: remove <domain_name>")
-            return
+    def remove(self, domain: str):
+        """Remove a domain: remove <domain>"""
+        self.dorch.remove_domain(domain)
 
-        self.dorch.remove_domain(line.strip())
-
-    def do_list(self, line):
+    def list(self):
         """List all active domains"""
         print("Active domains:", self.dorch.domainDictionary)
 
-    def do_pause(self, line):
-        """Pause a running domain: pause <domain_name>"""
-        if not line.strip():
-            print("Usage: pause <domain_name>")
-            return
+    def pause(self, domain: str):
+        """Pause a running domain: pause <domain>"""
+        self.dorch.pause_domain(domain)
 
-        self.dorch.pause_domain(line.strip())
+    def resume(self, domain: str):
+        """Resume a paused domain: resume <domain>"""
+        self.dorch.resume_domain(domain)
 
-    def do_resume(self, line):
-        """Resume a paused domain: resume <domain_name>"""
-        if not line.strip():
-            print("Usage: resume <domain_name>")
-            return
+    def run(self):
+        """Runs the typer app as we are calling it inside of a class"""
+        self.app()
 
-        self.dorch.resume_domain(line.strip())
-
-    def do_exit(self, line):
-        """Exit the shell and save all of the items"""
-        self.dorch.shutdown_applications()
-        return True
-
-    def do_quit(self, line):
-        """Exit the shell"""
-        self.dorch.shutdown_applications()
-        return True
-
-    def do_listen(self, line):
-        """listens to a broadcasting redis stream in order to obtain messages"""
-        args = line.strip().split()
+    def listen(self, domain):
+        """listens to a broadcasting redis stream in order to obtain messages for a specific domain"""
 
         # List the available streams to lisen to, these are all active domains that are currently running
         available_streams = [
@@ -97,54 +80,53 @@ class PyWebC2Shell(cmd.Cmd):
         ]
 
         # Append the all stream to the list of the available streams if more than two streams exist
-        if len(available_streams) > 2:
-            available_streams.append("all")
-
-        if not args:
-            print("Stream that wanted to be monitored was not inputted")
-            print("Use listen -l to see a list of the available streams to listen to")
-            return
-
-        # Flag that lists all of hte available streams
-        if args[0] == "-l":
-            if available_streams:
-                print("Available streams:")
-                print(*available_streams, sep="\n")
-                return
-            else:
-                print(
-                    "No active domains. Use 'create <domain>' to create a domain or resume <domain> to resume a previous isntance"
-                )
-                return
-
-        # Flag that can be used to access the log of a given stream
-        if args[0] == "h":
-            pass
-
+        #         if len(available_streams) > 2:
+        #             available_streams.append("all")
+        #
+        #         # Flag that lists all of hte available streams
+        # #        if args[0] == "-l":
+        #             if available_streams:
+        #                 print(*available_streams, sep="\n")
+        #                 return
+        #             else:
+        #                 print(
+        #                     "No active domains. Use 'create <domain>' to create a domain or resume <domain> to resume a previous isntance"
+        #                 )
+        #                 return
+        #
+        #         # Flag that can be used to access the log of a given stream
+        #         if args[0] == "-h":
+        #             if args[1] in available_streams:
+        #                 pass
+        #             else:
+        #                 print(
+        #                     f"{args[1]}, is not a valid stream, Syntax is listen -h <channel>"
+        #                 )
+        #
         # After these checks, it is assumed that the user passed in a domain to check, so:
-        if args[0] in available_streams:
-            stream_name = args[0]
-            print(f"Listening to stream '{stream_name}'. Press Ctrl+C to stop.")
+        stream_name = domain
+        print(f"Listening to stream '{stream_name}'. Press Ctrl+C to stop.")
 
-            try:
-                last_id = "$"  # Start from new messages only
-                while True:
-                    # Get new messages from the stream
-                    messages = self.redis_client.xread(
-                        {stream_name: last_id}, count=1, block=1000
-                    )
+        try:
+            last_id = "$"  # Start from new messages only
+            while True:
+                # Get new messages from the stream
+                messages = self.redis_client.xread(
+                    {stream_name: last_id}, count=1, block=1000
+                )
 
-                    if messages:
-                        for stream, msgs in messages:  # type: ignore
-                            for msg_id, fields in msgs:
-                                message_data = fields[b"message"].decode("utf-8")
-                                print(f"[{stream_name}] {message_data}")
-                                last_id = msg_id
+                if messages:
+                    for stream, msgs in messages:  # type: ignore
+                        for msg_id, fields in msgs:
+                            message_data = fields[b"message"].decode("utf-8")
+                            print(f"[{stream_name}] {message_data}")
+                            last_id = msg_id
 
-            except KeyboardInterrupt:
-                print(f"\nStopped listening to Redis stream '{stream_name}'")
+        except KeyboardInterrupt:
+            print(f"\nStopped listening to Redis stream '{stream_name}'")
 
-    # All this below code handles shutdowns in the case of an error or any other keyboard interruption. So that any actively running servers shut off appropriately
+    ## Functions for the graceful shutting down of the application in the case of an error ##
+
     def _register_shutdown_handlers(self):
         """Register handlers to ensure graceful shutdown on various exit scenarios"""
         # Register atexit handler for normal Python exit
@@ -171,7 +153,9 @@ class PyWebC2Shell(cmd.Cmd):
 
 
 def main():
-    PyWebC2Shell().cmdloop()
+    shell = PyWebC2Shell()
+    # Run application for typer shell
+    shell.run()
 
 
 if __name__ == "__main__":
